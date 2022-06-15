c5284672138987b415745a613e85d96b7a011bc8
midorigame737 <midorigame6@gmail.com>
2022-06-16 00:23:12 +0900

    実習に必要なファイル

---
+++ hoge.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"hoge.h"
+int main(){
+    int n;
+    scanf("%d",&n);
+    Stair(n);
+    Square(n);
+}
---
+++ hoge.h
@@ -0,0 +1,2 @@
+void Stair(int n);//n段の*の階段を出力する関数
+void Square(int n);//n段の*の正方形
